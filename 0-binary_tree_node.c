22307a2ac1626359d7871445441cb805b7f327a4
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-11-02 19:22:00 +0100

    Commiting to clone on windows

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,15 +8,27 @@
  * @value: The value of the node
  * Return: Returns pointer to the new node of NUll
  **/
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
-  binary_tree_t *new_node;
-  
-  if (*parent == NULL)
-    {
-      parent->n = value;
-    }
-  new_node = malloc(sizeof(binary_tree_t));
-  new_node->n = value;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
 
-  return new_node;
+	if (parent == NULL)
+	{
+		parent->n = value;
+		parent->left = NULL;
+		parent->right = NULL;
+	}
+	new_node = malloc(sizeof(binary_tree_t));
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	if (parent->left == NULL)
+	{
+		parent->left = new_node;
+	}
+	else if (parent->right == NULL)
+	{
+		parent->right = new_node;
+	}
+	return (new_node);
 }
