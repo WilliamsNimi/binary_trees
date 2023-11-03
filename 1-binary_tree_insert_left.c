9cfc55677a7374c605dd3ff91e9d71cfd28cd575
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-11-03 23:06:50 +0100

    Fixed issues in add tree left and right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
@@ -27,7 +32,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else if (parent->left != NULL)
 	{
-		new_node->left = parent->left->left;
+		new_node->left = parent->left;
 		parent->left = new_node;
 	}
 	return (new_node);
