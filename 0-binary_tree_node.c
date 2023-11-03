92ddcd7d3b693e588bdbe0e059222867038e0315
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-11-03 22:28:55 +0100

    Fixed Betty errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		free(new_node);
+		return (NULL);
 	}
 	new_node->parent = parent;
 	new_node->n = value;
